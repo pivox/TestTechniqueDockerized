a0d97711bff75dba693dbc818178c3bdf4d98d77
Haythem Mabrouk <haythem.mabrouk@gmail.com>
2020-04-13 23:37:43 +0200

    Dockorization

--- database/init.sql
+++
@@ -1,7 +0,0 @@
--- Database: smart_tribune_haythem_mabrouk
-
-DROP DATABASE IF EXISTS smart_tribune_haythem_mabrouk;
-DROP DATABASE IF EXISTS test_smart_tribune_haythem_mabrouk;
-
-CREATE DATABASE smart_tribune_haythem_mabrouk;
-CREATE DATABASE test_smart_tribune_haythem_mabrouk;
